bb3154c10ea6950a2836f6d410a0f3580f4357a8
chrisOburu <chrizoburu65@gmail.com>
2024-05-22 13:44:21 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # write fibonacci sequence of length length
+    if length == 0:
+        return []
+    else:
+        fibonacci_sequence = [0, 1]
+
+        for i in range(2, length):
+            fibonacci_sequence.append(fibonacci_sequence[i-1] + fibonacci_sequence[i-2])
+
+        return fibonacci_sequence
+
+
+print(print_fibonacci(0))
